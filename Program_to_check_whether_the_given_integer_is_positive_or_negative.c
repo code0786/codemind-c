a03306395cea79c8e3a249871708aa9aa3c7ece9
Ahsan <77656200+code0786@users.noreply.github.com>
2022-03-03 00:17:10 +0530

    #include<stdio.h> int main() {     float a;     scanf("%f",&a);     if (a>=0)     {         printf("Positive Number");     }     else     {         printf("Negative Number");     } }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    float a;
+    scanf("%f",&a);
+    if (a>=0)
+    {
+        printf("Positive Number");
+    }
+    else 
+    {
+        printf("Negative Number");
+    }
+}

25c8ce010a7b08feb674a849666238e1be498c28
Ahsan <77656200+code0786@users.noreply.github.com>
2022-05-30 23:35:20 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int arr[a],i;     for(i=0;i<a;i++)     {         scanf("%d",&arr[i]);     }     int sum=0;     for(i=0;i<a;i++)     {         if(arr[i]%2!=0)         {             sum+=arr[i];         }     }     printf("%d",sum);     return 0; }

---
+++ ODD_Sum.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int arr[a],i;
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int sum=0;
+    for(i=0;i<a;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            sum+=arr[i];
+        }
+    }
+    printf("%d",sum);
+    return 0;
+}

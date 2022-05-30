933113f93352be47efab2d1578f658348fc494e5
Ahsan <77656200+code0786@users.noreply.github.com>
2022-05-31 00:13:15 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int arr[a],i;     for(i=0;i<a;i++)     {         scanf("%d",&arr[i]);     }     int index=0;     for(i=0;i<a;i++)     {         if(arr[i]%2!=0)         {             index=i;         }     }     printf("%d",index);     return 0; }

---
+++ last_odd_number_index_position.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int arr[a],i;
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int index=0;
+    for(i=0;i<a;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            index=i;
+        }
+    }
+    printf("%d",index);
+    return 0;
+}

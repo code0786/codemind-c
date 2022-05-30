846baf38093dcce9374da4b5c6d9409cbfb5d8d6
Ahsan <77656200+code0786@users.noreply.github.com>
2022-05-30 23:24:24 +0530

    #include<stdio.h> int main() {     int a;     scanf("%d",&a);     int arr[a],i;     for(i=0;i<a;i++)     {         scanf("%d",&arr[i]);     }     int max=arr[0];     for(i=0;i<a;i++)     {         if(arr[i]>max)         {             max=arr[i];         }     }     printf("%d",max);     return 0; }

---
+++ maximum_of_an_array.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int arr[a],i;
+    for(i=0;i<a;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int max=arr[0];
+    for(i=0;i<a;i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+    }
+    printf("%d",max);
+    return 0;
+}

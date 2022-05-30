317857ee3e1c90afa54cbd8dfbcf8a6c2fac6d19
Ahsan <77656200+code0786@users.noreply.github.com>
2022-05-30 22:48:02 +0530

    #include<stdio.h> int main() {     int a,b;     scanf("%d%d",&a,&b);     int hcf=1,i,max;     if(a>b)     {         max=a;     }     else     {         max=b;     }     for(i=1;i<max;i++)     {         if(a%i==0 && b%i==0)         {             if(i>hcf)             {                 hcf=i;             }         }     }     printf("%d",hcf);     return 0; }

--- GCD_or_HCF.c
+++ GCD_or_HCF.c
@@ -1,16 +1,27 @@
 #include<stdio.h>
 int main()
 {
-    int a,b,t,i=0;
+    int a,b;
     scanf("%d%d",&a,&b);
-    for(t=2;t<=a&&t<=b;t++)
+    int hcf=1,i,max;
+    if(a>b)
     {
-        if(a%t==0&&b%t==0)
-        {i=t;}
+        max=a;
     }
-    if(i!=0)
-    printf("%d",i);
     else
-    printf("1");
+    {
+        max=b;
+    }
+    for(i=1;i<max;i++)
+    {
+        if(a%i==0 && b%i==0)
+        {
+            if(i>hcf)
+            {
+                hcf=i;
+            }
+        }
+    }
+    printf("%d",hcf);
     return 0;
 }

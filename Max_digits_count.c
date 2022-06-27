416a994257161695a793102a89c573e79a836598
Ahsan <77656200+code0786@users.noreply.github.com>
2022-06-28 00:13:43 +0530

    #include<stdio.h> int main() {     int n,c=0,count=0;     scanf("%d",&n);     int arr[n];     for (int i=0,d;i<n;i++)     {         d=0;         scanf("%d",&arr[i]);         int temp=arr[i];         while(temp)         {             d++;             temp/=10;         }         if (d>c){             c=d;         }     }     for (int i=0,d;i<n;i++)     {         d=0;         scanf("%d",&arr[i]);         while(arr[i])         {             d++;             arr[i]/=10;         }         if(d==c){             count++;         }     }     printf("%d",count); }

---
+++ Max_digits_count.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+int main()
+{
+    int n,c=0,count=0;
+    scanf("%d",&n);
+    int arr[n];
+    for (int i=0,d;i<n;i++)
+    {
+        d=0;
+        scanf("%d",&arr[i]);
+        int temp=arr[i];
+        while(temp)
+        {
+            d++;
+            temp/=10;
+        }
+        if (d>c){
+            c=d;
+        }
+    }
+    for (int i=0,d;i<n;i++)
+    {
+        d=0;
+        scanf("%d",&arr[i]);
+        while(arr[i])
+        {
+            d++;
+            arr[i]/=10;
+        }
+        if(d==c){
+            count++;
+        }
+    }
+    printf("%d",count);
+}

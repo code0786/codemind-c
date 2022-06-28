a8a39f5196c02617e8801bd1bb5efd1313e6e397
Ahsan <77656200+code0786@users.noreply.github.com>
2022-06-28 19:01:09 +0530

    #include<stdio.h> int main() {     int n,m,sum=0;     scanf("%d%d",&n,&m);     int arr[n][m];     for(int i=0;i<n;i++)     {         for(int j=0;j<m;j++)         {             scanf("%d",&arr[i][j]);             sum+=arr[i][j];         }     }     printf("%d",sum); }

---
+++ Sum_of_all_elements_in_a_matrix.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,m,sum=0;
+    scanf("%d%d",&n,&m);
+    int arr[n][m];
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            scanf("%d",&arr[i][j]);
+            sum+=arr[i][j];
+        }
+    }
+    printf("%d",sum);
+}

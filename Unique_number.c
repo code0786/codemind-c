b14103f8cc01b24d4c76f7c94bea68169d69ab31
Ahsan <77656200+code0786@users.noreply.github.com>
2022-06-29 21:14:23 +0530

    #include<stdio.h> main() {     int temp,zero=0,one=0,two=0,three=0,four=0,five=0,six=0,seven=0,eight=0,nine=0;     scanf("%d",&temp);     while(temp)     {         if(temp%10==0)         {             zero++;         }         else if(temp%10==1)         {             one++;         }         else if(temp%10==2)         {             two++;         }         else if(temp%10==3)         {             three++;         }         else if(temp%10==4)         {             four++;         }         else if(temp%10==5)         {             five++;         }         else if(temp%10==6)         {             six++;         }         else if(temp%10==7)         {             seven++;         }         else if(temp%10==8)         {             eight++;         }         else if(temp%10==9)         {             nine++;         }         temp/=10;     }     if(zero<2 && one<2 && two<2 && three<2 && four<2 && five<2 && six<2 && seven<2 && eight<2 && nine<2 )     {         printf("Unique Number");     }     else     {         printf("Not Unique Number");     } }

--- Unique_number.c
+++ Unique_number.c
@@ -1,21 +1,58 @@
 #include<stdio.h>
-int main(){
-    int n,i,digit;
-    static int arr[10];
-    scanf("%d",&n);
-    while(n>0){
-        digit = n%10;
-        arr[digit]++;
-        n=n/10;
-    }
-    for(i=0;i<10;i++){
-        if(arr[i]>1){
-            printf("Not Unique Number");
-            break;
+main()
+{
+    int temp,zero=0,one=0,two=0,three=0,four=0,five=0,six=0,seven=0,eight=0,nine=0;
+    scanf("%d",&temp);
+    while(temp)
+    {
+        if(temp%10==0)
+        {
+            zero++;
         }
+        else if(temp%10==1)
+        {
+            one++;
+        }
+        else if(temp%10==2)
+        {
+            two++;
+        }
+        else if(temp%10==3)
+        {
+            three++;
+        }
+        else if(temp%10==4)
+        {
+            four++;
+        }
+        else if(temp%10==5)
+        {
+            five++;
+        }
+        else if(temp%10==6)
+        {
+            six++;
+        }
+        else if(temp%10==7)
+        {
+            seven++;
+        }
+        else if(temp%10==8)
+        {
+            eight++;
+        }
+        else if(temp%10==9)
+        {
+            nine++;
+        }
+        temp/=10;
     }
-    if(i==10){
+    if(zero<2 && one<2 && two<2 && three<2 && four<2 && five<2 && six<2 && seven<2 && eight<2 && nine<2 )
+    {
         printf("Unique Number");
     }
-    return 0;
+    else
+    {
+        printf("Not Unique Number");
+    }
 }

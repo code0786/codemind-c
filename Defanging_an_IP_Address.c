d4b4dc583488485003f998bc551e058a6d7fea12
Ahsan <77656200+code0786@users.noreply.github.com>
2022-10-09 22:33:45 +0530

    #include<stdio.h>
    int main()
    {
        char arr[100];
        scanf("%[^
    ]s",arr);
        for(int i=0;arr[i]!=NULL;i++)
        {
            if(arr[i]=='.')
            printf("[.]");
            else
            printf("%d",arr[i]-48);
        }
        return 0;
    }

---
+++ Defanging_an_IP_Address.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    char arr[100];
+    scanf("%[^
+]s",arr);
+    for(int i=0;arr[i]!=NULL;i++)
+    {
+        if(arr[i]=='.')
+        printf("[.]");
+        else
+        printf("%d",arr[i]-48);
+    }
+    return 0;
+}
